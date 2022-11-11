a7f9e2a160b2efe174c7e9a216141e98f66ed5d7
John Thomas Menchaca <jt.menchaca@gmail.com>
2022-11-11 12:08:31 -0500

    Initial commit

---
+++ R/package_create.R
@@ -0,0 +1,14 @@
+library(usethis)
+create_package(getwd())
+use_mit_license("John Thomas Menchaca")
+
+
+library(tidyverse)
+options(dplyr.summarise.inform = FALSE)
+library(readxl)
+library(writexl)
+library(janitor)
+library(broom)
+library(glue)
+library(scales)
+library(fastDummies)
