1bf6de985fceb10bcb99f4c630f6df31b4874ed0
John Thomas Menchaca <jt.menchaca@gmail.com>
2022-11-11 12:22:45 -0500

    Initial commit

--- R/package_create.R
+++ R/package_create.R
@@ -1,20 +1,20 @@
-library(devtools)
-library(usethis)
-#https://r-pkgs.org/whole-game.html
-create_package(getwd())
-use_git()
-
-
-
-use_mit_license("John Thomas Menchaca")
-
-
-library(tidyverse)
-options(dplyr.summarise.inform = FALSE)
-library(readxl)
-library(writexl)
-library(janitor)
-library(broom)
-library(glue)
-library(scales)
-library(fastDummies)
+# library(devtools)
+# library(usethis)
+# #https://r-pkgs.org/whole-game.html
+# # create_package(getwd())
+# # use_git()
+#
+#
+#
+# # use_mit_license("John Thomas Menchaca")
+#
+#
+# library(tidyverse)
+# options(dplyr.summarise.inform = FALSE)
+# library(readxl)
+# library(writexl)
+# library(janitor)
+# library(broom)
+# library(glue)
+# library(scales)
+# library(fastDummies)
